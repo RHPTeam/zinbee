c8ad74cbdc948165bdac4fdc768fb701459d1f77
hoc-anms <phamvanhoc96@gmail.com>
2019-06-05 12:07:54 +0700

    update api forget password

--- server/src/microservices/synchronize/account.js
+++ server/src/microservices/synchronize/account.js
@@ -7,5 +7,14 @@ module.exports = {
       "url": url,
       "data": data
     } );
+  },
+  "createNewPasswordSync": ( region, url, data, headers ) => {
+    return request( {
+      "method": "post",
+      // eslint-disable-next-line no-nested-ternary
+      "url": region === 0 ? `${process.env.SERVER_CHILDRENT_NORTH }/` + url : region === 1 ? `${process.env.SERVER_CHILDRENT_CENTRAL }/` + url : `${process.env.SERVER_CHILDRENT_SOUTH }/` + url,
+      "data": data,
+      "headers": headers
+    } );
   }
 };
