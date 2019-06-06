e42d2ce4361cf77815ddb6e45b1747aa1aa1a594
khanhfood <congkhanh98.hp@gmail.com>
2019-06-07 02:01:12 +0700

    upload delete attribute create product

--- client/src/views/admin/market/product/create/index.vue
+++ client/src/views/admin/market/product/create/index.vue
@@ -8,7 +8,11 @@
           <div class="btn btn_info" @click="showOptionsCampaign">Campaign</div>
         </div>
         <div class="py_0 post" v-if="isOptionsPost === true">
-          <multiselect label="name" class="form_control p_0" placeholder="Chọn bài de đăng..."/>
+          <multiselect
+            label="name"
+            class="form_control p_0"
+            placeholder="Chọn bài de đăng..."
+          />
           <div class="post mt_2">
             <label for class>Danh muc:</label>
             <div class="form_control py_0">
@@ -30,14 +34,21 @@
           <div class="campaign mt_2">
             <label for class>Danh muc:</label>
             <div class="py_0">
-              <multiselect label="name" placeholder="Chọn danh mục đăng bài..."/>
+              <multiselect
+                label="name"
+                placeholder="Chọn danh mục đăng bài..."
+              />
             </div>
           </div>
         </div>
       </div>
       <div class="form_group title">
         <label for class>Tieu de:</label>
-        <input type="text" class="form_control" v-model="inforProductById.name">
+        <input
+          type="text"
+          class="form_control"
+          v-model="inforProductById.name"
+        />
       </div>
       <div class="form_group d_flex align_items_center price">
         <label for class="mr_2 m_0">Gia:</label>
@@ -49,13 +60,16 @@
             placeholder="100.000 d"
             min="10"
             v-model="inforProductById.priceCents"
-          >
+          />
           <div class="pl_3 price--info">Don vi nay se tinh bang VND</div>
         </div>
       </div>
       <div class="form_group">
         <label class>Dac diem:</label>
-        <div v-for="(attr, index) in inforProductById.attributes" :key="`e-${index}`">
+        <div
+          v-for="(attr, index) in inforProductById.attributes"
+          :key="`e-${index}`"
+        >
           <div class="d_flex align_items_center mb_2">
             <div class="mr_2">
               <input
@@ -63,7 +77,7 @@
                 class="form_control"
                 placeholder="Name"
                 v-model="inforProductById.attributes[index].name"
-              >
+              />
             </div>
             <div class="mr_2">
               <input
@@ -71,7 +85,7 @@
                 class="form_control"
                 placeholder="Value"
                 v-model="inforProductById.attributes[index].value"
-              >
+              />
             </div>
             <div @click="deleteAttribute(index)">
               <icon-base
@@ -81,12 +95,15 @@
                 height="20"
                 viewBox="0 0 16 16"
               >
-                <icon-remove/>
+                <icon-remove />
               </icon-base>
             </div>
           </div>
         </div>
-        <div class="d_inline_flex mb_2 align_items_center add--att p_1" @click="addAttribute">
+        <div
+          class="d_inline_flex mb_2 align_items_center add--att p_1"
+          @click="addAttribute"
+        >
           <icon-base
             icon-name="icon-plus"
             class="icon-plus"
@@ -94,27 +111,37 @@
             height="25"
             viewBox="0 0 60 70"
           >
-            <icon-plus/>
-          </icon-base>Thêm dac diem
+            <icon-plus /> </icon-base
+          >Thêm dac diem
         </div>
       </div>
       <div class="form_group">
         <label for class>Anh dai dien:</label>
         <div class>
-          <input type="file" class="form_control p_1">
+          <input type="file" class="form_control p_1" />
         </div>
         <div class="contain--images"></div>
       </div>
       <div class="form_group">
         <label for class>Mo ta bai viet:</label>
         <div class>
-          <textarea rows="5" cols="10" class="form_control" v-model="inforProductById.description"></textarea>
+          <textarea
+            rows="5"
+            cols="10"
+            class="form_control"
+            v-model="inforProductById.description"
+          ></textarea>
         </div>
       </div>
       <div class="form_group">
         <label for class>Noi dung khi xem thu:</label>
         <div class>
-          <textarea rows="5" cols="10" class="form_control" v-model="inforProductById.content"></textarea>
+          <textarea
+            rows="5"
+            cols="10"
+            class="form_control"
+            v-model="inforProductById.content"
+          ></textarea>
         </div>
       </div>
       <div class="form_group">
@@ -127,8 +154,20 @@
           />
         </div>
       </div>
-      <div class="btn btn_warning" @click="updateProduct" v-if="nodeUpdate === 0">Update</div>
-      <div class="btn btn_success" @click="createProduct" v-if="nodeUpdate === 1">Tao moi</div>
+      <div
+        class="btn btn_warning"
+        @click="updateProduct"
+        v-if="nodeUpdate === 0"
+      >
+        Update
+      </div>
+      <div
+        class="btn btn_success"
+        @click="createProduct"
+        v-if="nodeUpdate === 1"
+      >
+        Tao moi
+      </div>
     </div>
   </div>
 </template>
