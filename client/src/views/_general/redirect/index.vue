c16b7dcbc937c90330e8d3b0c159ec97d052f4fe
Sky Albert <skyalbert@Skys-MacBook-Pro.local>
2019-06-10 05:33:18 +0700

    update fix function logout zinbee

--- client/src/views/_general/redirect/index.vue
+++ client/src/views/_general/redirect/index.vue
@@ -5,9 +5,15 @@
 </template>
 
 <script>
+  import CookieFunction from "@/utils/functions/cookie";
 export default {
   async created() {
-    await localStorage.clear();
+    CookieFunction.removeCookie( "sid" );
+    CookieFunction.removeCookie( "uid" );
+    CookieFunction.removeCookie( "cfr" );
+    CookieFunction.removeCookie( "token" );
+    CookieFunction.removeCookie( "_sub" );
+    CookieFunction.removeCookie( "__v" );
     this.$router.push({ name: "user_signin" });
   }
 };
