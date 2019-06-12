bcda5a563e0fdb9b524794812afbd5a0faa902de
Sky Albert <skyalbert@Skys-MacBook-Pro.local>
2019-06-12 16:40:14 +0700

    Update file before - redirect logout

--- client/src/views/_general/redirect/index.vue
+++ client/src/views/_general/redirect/index.vue
@@ -5,15 +5,15 @@
 </template>
 
 <script>
-  import CookieFunction from "@/utils/functions/cookie";
+import CookieFunction from "@/utils/functions/cookie";
 export default {
   async created() {
-    CookieFunction.removeCookie( "sid" );
-    CookieFunction.removeCookie( "uid" );
-    CookieFunction.removeCookie( "cfr" );
-    CookieFunction.removeCookie( "token" );
-    CookieFunction.removeCookie( "_sub" );
-    CookieFunction.removeCookie( "__v" );
+    CookieFunction.removeCookie("sid");
+    CookieFunction.removeCookie("uid");
+    CookieFunction.removeCookie("cfr");
+    CookieFunction.removeCookie("token");
+    CookieFunction.removeCookie("_sub");
+    CookieFunction.removeCookie("__v");
     this.$router.push({ name: "user_signin" });
   }
 };
