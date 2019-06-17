d53d98ae5880a27072e01cf18420450f6ba58d5e
MondVn <trannamanh94@gmail.com>
2019-06-17 10:49:16 +0700

    commit before merge code

--- client/src/views/admin/market/product/index.vue
+++ client/src/views/admin/market/product/index.vue
@@ -20,31 +20,33 @@
             <div></div>
           </div>
           <!-- Header Content -->
-          <div
-            class="d_flex content"
-            v-for="(product, index) in products"
-            :key="index"
-          >
-            <div class="kind--of--item p_2">{{ product.name }}</div>
-            <div class="category--item p_2">{{ product._category.name }}</div>
-            <div class="tag--item p_2">
+          <div v-if="products && products.length > 0">
+            <div
+              class="d_flex content"
+              v-for="(product, index) in products"
+              :key="index"
+            >
+              <div class="kind--of--item p_2" v-if="product.name && product.name.length > 0">{{ product.name }}</div>
+              <div class="category--item p_2" v-if="product._category && product._category.length > 0">{{ product._category.name }}</div>
+              <div class="tag--item p_2">
               <span v-for="(tag, index) in product.tags" :key="`a-${index}`"
-                >{{ tag
+              >{{ tag
                 }}{{ index === product.tags.length - 1 ? "" : ", " }}</span
               >
-            </div>
-            <div class="price--item p_2">{{ product.priceCents }}</div>
-            <div class="price--item p_2">{{ product._creator.name }}</div>
-            <div class="action p_2 d_flex">
-              <div class="btn btn_primary mr_1" @click="goToUpdate(product)">
-                Update
               </div>
-              <div class="btn btn_danger" @click="deleteProduct(product._id)">
-                Delete
+              <div class="price--item p_2" v-if="product.priceCents && product.priceCents.length > 0">{{ product.priceCents }}</div>
+              <div class="price--item p_2" v-if="product._creator && product._creator.length > 0">{{ product._creator.name }}</div>
+              <div class="action p_2 d_flex">
+                <div class="btn btn_primary mr_1" @click="goToUpdate(product)">
+                  Update
+                </div>
+                <div class="btn btn_danger" @click="deleteProduct(product._id)">
+                  Delete
+                </div>
               </div>
             </div>
           </div>
-          <div class="p_2 text_center" v-if="products.length === 0">
+          <div class="p_2 text_center" v-else>
             Không có sản phẩm nào
           </div>
         </div>
