84e9c5194d46d8f37bd7edee63946966c558235d
yendang <dangthiyen103@gmail.com>
2019-06-19 20:16:51 +0700

    [Expiration] Function + Layout v01

---
+++ client/src/views/_general/expiration/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <!-- Start: Header -->
+    <app-header></app-header>
+    <!-- End: Header -->
+    <!-- Start: Main -->
+    <app-main></app-main>
+    <!-- End: Main -->
+  </div>
+</template>
+
+<script>
+import AppHeader from "./header";
+import AppMain from "./main";
+
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+  background-color: #F7F7F7;
+}
+</style>
