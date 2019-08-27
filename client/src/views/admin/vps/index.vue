c0888191e0238402bb11e7671b726bfa595cd951
Le Khang <khanggle0608@gmail.com>
2019-08-27 17:18:21 +0700

    update file v77 then add modules post server and update structure modules vps

--- client/src/views/admin/vps/index.vue
+++ client/src/views/admin/vps/index.vue
@@ -5,27 +5,28 @@
     </div>
     <div class="action py_2 px_2">
       <button class="btn btn_info" @click="redirectVps">Quản lý VPS</button>
-      <button class="btn btn_dark ml_3" @click="redirectPostServer">Quản lý server post</button>
+      <button class="btn btn_dark ml_3" @click="redirectPostServer">
+        Quản lý server post
+      </button>
     </div>
 
     <div>
       <router-view />
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    redirectVps(){
-      this.$router.push( {name: "admin_server"} );
+    redirectVps() {
+      this.$router.push({ name: "admin_server" });
     },
-    redirectPostServer(){
-      this.$router.push( {name: "post_server"} );
+    redirectPostServer() {
+      this.$router.push({ name: "post_server" });
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped></style>
