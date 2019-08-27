36e963e4ed866cc49068f0f7060ad44ebcef638c
Sky Albert <trantoan.fox.97@gmail.com>
2019-08-27 11:04:18 +0700

    Update file v80 - update post server api to scale project

---
+++ server/src/routes/modules/postserver.route.js
@@ -0,0 +1,22 @@
+/**
+ * Create route Post Server for project
+ * author: sky albert
+ * date: 21/05/2019
+ * team: BE-RHP
+ */
+const router = require( "express-promise-router" )();
+
+const PostServerController = require( "../../controllers/postserver.controller" );
+const auth = require( "../../helpers/middleware/authenticate.middleware" );
+const permission = require( "../../helpers/middleware/permission.middleware" );
+
+router
+  .route( "/" )
+  .get( auth, permission, PostServerController.index )
+  .post( auth, permission, PostServerController.create )
+  .patch( auth, permission, PostServerController.update )
+  .delete( auth, permission, PostServerController.delete );
+
+router.route( "/:uid/status" ).post( PostServerController.changeStatus );
+
+module.exports = router;
